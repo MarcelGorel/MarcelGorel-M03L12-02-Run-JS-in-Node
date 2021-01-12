c99dcdf3b5542b7997270d029470dbc4be9fee53
MarcelGorel <66006966+MarcelGorel@users.noreply.github.com>
2021-01-12 10:01:38 +0100

    Add files via upload

---
+++ Opdracht-2/script.js
@@ -0,0 +1,12 @@
+console.log('Hallo Winc Academy studenten');
+//Dit is een grote som.
+/*
+let multiply = 1230941 * 1823794;
+let devide = 637263 / 54;
+*/
+console.log(multiply);
+console.log(devide);
+console.log("je ziet de comment niet in de console omdat je die uitsluit door de comment te maken")
+console.log("JS ziet de blokcomment niet en berekend dus ook niets.")
+
+
